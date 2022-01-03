56a1380d24cfea94d5948cee63355e5e3570570f
Václav Dvorský <vaclav.dvorsky@inventi.cz>
2022-01-03 16:01:04 +0100

    install Docker Compose v2

--- ubuntu_install_2.sh
+++ ubuntu_install_2.sh
@@ -1,9 +1,9 @@
 #   *********************************************
 #   script for NTB install - Ubuntu 20.04, part 2
 #   begin     : Fri 25 Sep 2020.
-#   copyright : (c) 2021 Václav Dvorský
+#   copyright : (c) 2022 Václav Dvorský
 #   email     : vaclav.dvorsky@hotmail.com
-#   $Id: ubuntu_install.sh, v3.02 20/01/2021
+#   $Id: ubuntu_install.sh, v3.20 03/01/2022
 #   *********************************************
 #
 #   --------------------------------------------------------------------
@@ -19,22 +19,23 @@ if ! [ $(id -u) = 0 ]; then
     docker run hello-world
     docker ps -a
     systemctl is-enabled docker
-    sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    # install Docker Compose v2
+    sudo curl -L "https://github.com/docker/compose/releases/download/v2.2.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
     sudo chmod +x /usr/local/bin/docker-compose
-    sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
-    docker-compose --version
+    mkdir -p $HOME/.docker/cli-plugins/
+    ln -s /usr/local/bin/docker-compose $HOME/.docker/cli-plugins/docker-compose
+    docker compose version
     sudo apt-get install git
     sudo apt-get install -y openvpn 
     sudo apt-get install -y network-manager-openvpn
     sudo apt-get install -y network-manager-openvpn-gnome openvpn-systemd-resolved
     sudo apt-get -f install && sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get dist-upgrade -y && sudo apt-get autoremove -y
-    #nastavíme sítě
-    #cd network
+    # set up networks
     sudo rsync -a ca.crt client.crt client.key login *.ovpn /etc/openvpn/client
     sudo chmod 400 /etc/openvpn/client/*
-    #sudo nmcli connection import type openvpn file /etc/openvpn/client/vpn.ovpn
+    # sudo nmcli connection import type openvpn file /etc/openvpn/client/vpn.ovpn
     sudo nano /etc/hostname
-    #věci další a nedůležité
+    # other and unimportant things
     sudo apt-get install -y sysstat
     sudo systemctl enable sysstat
     sudo sed -i 's/false/true/g' /etc/default/sysstat
