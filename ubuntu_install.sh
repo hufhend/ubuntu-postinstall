2fe0ca212b634a32e9f0fcff1e95bdf3c1a37224
Václav Dvorský <82517261+hufhend@users.noreply.github.com>
2022-08-17 00:44:26 +0200

    Update ubuntu_install.sh
    Add new apt-key
    before: https://download.docker.com/linux/ubuntu/dists/focal/InRelease: Key is stored in legacy trusted.gpg keyring (/etc/apt/trusted.gpg), see the DEPRECATION section in apt-key(8) for details.

--- ubuntu_install.sh
+++ ubuntu_install.sh
@@ -20,9 +20,10 @@ if ! [ $(id -u) = 0 ]; then
     sudo ufw enable
     # instalace dockeru
     sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-    # sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu  $(lsb_release -cs)  stable"
-    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+    echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+    $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
     sudo apt-get update
     apt-cache policy docker-ce
     sudo apt install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
