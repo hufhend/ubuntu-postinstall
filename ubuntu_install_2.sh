16775d6e073b2e0c99d0a38db4a77a6d5b88fe6f
Václav Dvorský <82517261+hufhend@users.noreply.github.com>
2022-09-14 22:42:53 +0200

    Update ubuntu_install_2.sh

--- ubuntu_install_2.sh
+++ ubuntu_install_2.sh
@@ -1,9 +1,9 @@
 #   *********************************************
 #   script for NTB install - Ubuntu 20.04, part 2
-#   begin     : Fri 25 Sep 2020.
+#   begin     : Fri 25 Sep 2020
 #   copyright : (c) 2022 Václav Dvorský
 #   email     : vaclav.dvorsky@hotmail.com
-#   $Id: ubuntu_install.sh, v3.20 03/01/2022
+#   $Id: ubuntu_install.sh, v3.25 14/09/2022
 #   *********************************************
 #
 #   --------------------------------------------------------------------
@@ -20,19 +20,19 @@ if ! [ $(id -u) = 0 ]; then
     docker ps -a
     systemctl is-enabled docker
     # install Docker Compose v2
-    sudo curl -L "https://github.com/docker/compose/releases/download/v2.0.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-    sudo chmod +x /usr/local/bin/docker-compose
-    mkdir -p $HOME/.docker/cli-plugins/
-    ln -s /usr/local/bin/docker-compose $HOME/.docker/cli-plugins/docker-compose
+    # sudo curl -L "https://github.com/docker/compose/releases/download/v2.11.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    # sudo chmod +x /usr/local/bin/docker-compose
+    # mkdir -p $HOME/.docker/cli-plugins/
+    # ln -s /usr/local/bin/docker-compose $HOME/.docker/cli-plugins/docker-compose
     docker compose version
-    sudo apt-get install git
-    sudo apt-get install -y openvpn 
+    sudo apt-get install git mc
+    sudo apt-get install -y openvpn
     sudo apt-get install -y network-manager-openvpn
     sudo apt-get install -y network-manager-openvpn-gnome openvpn-systemd-resolved
     sudo apt-get -f install && sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get dist-upgrade -y && sudo apt-get autoremove -y
     # set up networks
-    sudo rsync -a ca.crt client.crt client.key login *.ovpn /etc/openvpn/client
-    sudo chmod 400 /etc/openvpn/client/*
+    # sudo rsync -a ca.crt client.crt client.key login *.ovpn /etc/openvpn/client
+    # sudo chmod 400 /etc/openvpn/client/*
     # sudo nmcli connection import type openvpn file /etc/openvpn/client/vpn.ovpn
     sudo nano /etc/hostname
     # other and unimportant things
