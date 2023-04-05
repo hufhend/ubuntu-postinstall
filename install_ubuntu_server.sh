b6a7c8e36af58a2d069bf6ed5c37015608e3c160
Václav Dvorský <hufhendr@gmail.com>
2023-04-05 22:33:49 +0200

    Add server installation

---
+++ install_ubuntu_server.sh
@@ -0,0 +1,37 @@
+#   ***********************************************
+#   script for server install - Ubuntu 22.04
+#   begin     : Wed 5 Apr 2023
+#   copyright : (c) 2023 Václav Dvorský
+#   email     : vaclav.dvorsky@hotmail.com
+#   $Id: install_ubuntu_server.sh, v1.00 05/04/2023
+#   ***********************************************
+#
+#   --------------------------------------------------------------------
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public Licence as published by
+#   the Free Software Foundation; either version 2 of the Licence, or
+#   (at your option) any later version.
+#   --------------------------------------------------------------------
+
+#!/bin/bash
+if ! [ $(id -u) = 0 ]; then
+    net=192.168.0.0/21
+    # complete updates
+    sudo apt -f install && sudo apt update && sudo apt upgrade -y && sudo apt dist-upgrade -y && sudo apt autoremove -y
+    sudo apt install -y ssh 
+    sudo ufw enable
+    sudo ufw allow from $net to any port ssh comment 'Open ssh port 22'
+    sudo nano /etc/hostname
+    # installing useful SW
+    sudo apt install -y htop screen mc sysstat smartmontools lm-sensors fail2ban
+    # sudo apt install keepalived
+    sudo sed -i 's/false/true/g' /etc/default/sysstat
+    sudo systemctl enable sysstat smartmontools
+    sudo systemctl start sysstat
+    # install monitoring service
+    sudo apt-get install -y prometheus-node-exporter
+    sudo ufw allow from $net to any port 9100 proto tcp comment 'Open node-exporter port 9100'
+  exit
+fi
+    #here go superuser commands
+    echo This script cannot run root
