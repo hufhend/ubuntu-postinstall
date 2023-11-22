0720c394469ee2d2fc20af74560b6f9e4049bf6f
Václav Dvorský <hufhendr@gmail.com>
2023-11-22 18:10:59 +0100

    add parameter for master and worker node auto-complete the name of the network card branching the script by installation type

--- cluster-node-install.sh
+++ cluster-node-install.sh
@@ -3,7 +3,7 @@
 #   begin     : Wed 5 Apr 2023
 #   copyright : (c) 2023 Václav Dvorský
 #   email     : vaclav.dvorsky@hotmail.com
-#   $Id: cluster-node-install.sh, v1.11 18/11/2023
+#   $Id: cluster-node-install.sh, v1.20 22/11/2023
 #   **********************************************
 #
 #   --------------------------------------------------------------------
@@ -19,7 +19,18 @@
 #   Using NTB makes sense in terms of price and power consumption. Gnome may be useful if repairs
 #   are needed. The NTB also has a display, keyboard, mouse and its own UPS.
 
-#!/bin/bash
+#!/bin/sh
+
+# check if exactly one parameter is provided
+if [ "$#" -ne 1 ]; then
+  echo "Error: The script requires exactly one parameter (master or worker)."
+  exit 1
+fi
+
+# assign the value of the parameter to the 'role' variable
+role="$1"
+ether=$(ip link show | awk '/^[0-9]+: [^l]/ && !/^ *[0-9]+: wl/ {print $2}' | sed 's/:$//')
+
 if ! [ $(id -u) = 0 ]; then
     net=192.168.0.0/21
     user=${USER}
@@ -29,6 +40,7 @@ if ! [ $(id -u) = 0 ]; then
     sudo nano /etc/hostname
 
     # install useful SW
+    sudo apt install -y apt-transport-https ca-certificates curl
     sudo apt install -y htop screen mc sysstat smartmontools lm-sensors fail2ban open-iscsi nfs-common ethtool
     sudo sed -i 's/false/true/g' /etc/default/sysstat
     sudo systemctl enable sysstat smartmontools
@@ -77,46 +89,25 @@ if ! [ $(id -u) = 0 ]; then
     # disable firewall rules
     sudo ufw disable
 
-    # uncomment to install keepalived
-    # sudo apt install -y keepalived
-    # # keepalived configuration
-    # content='vrrp_instance VI_1 {
-
-    #     state MASTER            # or BACKUP
-    #     interface enp8s0        # change the network card by "ip a"
-    #     virtual_router_id 51
-    #     priority 255            # reduce by one for each additional
-    #     advert_int 1
-    #     authentication {
-    #         auth_type PASS
-    #         auth_pass 12345
-    #     }
-    #     virtual_ipaddress {
-    #         192.168.3.110/24
-    #     }
-    # }'
-    # file_path="/etc/keepalived/keepalived.conf"
-    # echo "$content" > "pom.txt"
-    # sudo cp pom.txt "$file_path" && rm pom.txt
-
     # Wake-On-Lan Activation
-    content='[Unit]
+    content="[Unit]
 Description=Enable Wake-up on LAN
 
 [Service]
 Type=oneshot
-# Change to your card by "ip a"
-ExecStart=/sbin/ethtool -s enp8s0 wol g
+# Check the card using 'ip a'
+# Also need to enable WOL in the BIOS
+ExecStart=/sbin/ethtool -s $ether wol g
 
 [Install]
-WantedBy=basic.target'
+WantedBy=basic.target"
     file_path_wol="/etc/systemd/system/wol-enable.service"
     echo "$content" > "wol-enable.txt"
     sudo cp wol-enable.txt "$file_path_wol" && rm wol-enable.txt
     sudo systemctl enable wol-enable.service
     sudo systemctl start wol-enable.service
 
-    # Add new Crontab
+    # add new Crontab
     current_cron=$(crontab -l 2>/dev/null)
     new_cron="# For more information see the manual pages of crontab(5) and cron(8)
 # m h dom mon dow   command
@@ -140,13 +131,42 @@ WantedBy=basic.target'
     crontab /tmp/mycron
     rm /tmp/mycron
 
-    # # Install kubectl on worker node
-    # curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-archive-keyring.gpg
-    # echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
-    # sudo apt update
-    # sudo apt install kubectl
+    # here the installation depends on the selected node role
+    if [ "$role" = "master" ]; then
+        # install keepalived on master node
+        sudo apt install -y keepalived
+        # keepalived configuration
+        content="vrrp_instance VI_1 {
+
+            state MASTER            # or BACKUP
+            interface $ether        # check the card using 'ip a'
+            virtual_router_id 51
+            priority 255            # reduce by one for each additional
+            advert_int 1
+            authentication {
+                auth_type PASS
+                auth_pass 12345
+            }
+            virtual_ipaddress {
+                192.168.3.110/24
+            }
+        }"
+        file_path="/etc/keepalived/keepalived.conf"
+        echo "$content" > "pom.txt"
+        sudo cp pom.txt "$file_path" && rm pom.txt
+
+    elif [ "$role" = "worker" ]; then
+        # install kubectl on worker node
+        curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-archive-keyring.gpg
+        echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+        sudo apt update
+        sudo apt install kubectl
+    else
+    echo "Error: The parameter must be either master or worker."
+    exit 1
+    fi
 
   exit
 fi
-    #here go superuser commands
+    # here go superuser commands
     echo This script cannot run root
